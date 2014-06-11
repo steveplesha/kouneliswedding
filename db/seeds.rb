c8ba6b99218674ef1bc03eff10cc894357baf245
Steve Plesha <steveplesha@gmail.com>
2014-06-11 12:29:36 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Steve = User.new([{username: "splesha"}, {password: "splesha"}, {firstname1: "Steve"}, {lastname1: "Plesha"}, {firstname2: "Jen"}, {lastname2: "Plesha"}, {numguests: 0}, {maxguests: 0}, {role: "admin"})
